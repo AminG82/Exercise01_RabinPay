4e7b6409f9f079bbb9cd1f9f942ab08d3f03766d
Amin Ghasemi <mmnghasemi@gmail.com>
2024-08-03 02:10:45 -0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿
+//RabinPay Exercise01 :
+
+int arrayIndex;
+
+Console.Write("Input number of strings to store in the array : ");
+arrayIndex = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("\r\n");
+
+Console.WriteLine($"Input {arrayIndex} strings for the array :");
+
+string[] array = new string[arrayIndex];
+for (int i = 0; i < arrayIndex; i++)
+{
+    Console.Write($"Element[{i}] : ");
+    array[i] = Console.ReadLine();
+}
+
+Console.WriteLine("\r\n");
+
+Console.Write("Input the minimum length of the item you want to find : ");
+int minLength = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("\r\n");
+
+Console.WriteLine($"The items of minimum {minLength} characters are :");
+
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i].Length >= minLength)
+    {
+        Console.WriteLine($"Item: {array[i]}");
+    }
+}
+
+Console.ReadKey();
